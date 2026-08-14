bc17e985d4ea8765ac05dce209a6d2452dde210e
kent <kent.26pg@gmail.com>
2026-08-14 10:56:41 +0200

    added ThemeContext and coded it

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,17 @@
 import { TodoProvider } from "./context/TodoContext";
+import { ThemeProvider } from "./Context/ThemeContext";
 import AddTodo from "./Components/AddTodo";
 import TodoList from "./Components/TodoLIst";
 
 function App() {
   return (
+    <ThemeProvider>
     <TodoProvider>
       <h1>Todo App</h1>
       <AddTodo />
       <TodoList />
     </TodoProvider>
+    </ThemeProvider>
   );
 }
 
